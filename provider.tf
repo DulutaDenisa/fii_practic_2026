d7be2a43794e3bdf7ca2649a1fbc4762b7f51473
DulutaDenisa <dulutadenisa28@gmail.com>
2026-04-18 11:12:09 +0000

    versiune3

--- provider.tf
+++ provider.tf
@@ -17,9 +17,9 @@ terraform {
   }
 
   backend "s3" {
-    bucket  = "demodenisa"
+    bucket  = "denisademo"
     key     = "terraform.tfstate"
-    region  = "us-east-1"
+    region  = "us-east-2"
     encrypt = true
   }
 }
